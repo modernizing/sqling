829070a245c21f63bc05d86a1dccf21755b8f252
Phodal Huang <h@phodal.com>
2021-06-11 08:39:17 +0800

    feat: try to get values

--- main.go
+++ main.go
@@ -5,24 +5,35 @@ import (
 	"github.com/xwb1989/sqlparser"
 )
 
+type CocoStruct struct {
+	name   string
+	fields []string
+}
+
 func main() {
 	sql := "CREATE TABLE `cms_help` (\n  `id` bigint(20) NOT NULL AUTO_INCREMENT,\n  `category_id` bigint(20) DEFAULT NULL,\n  `icon` varchar(500) DEFAULT NULL,\n  `title` varchar(100) DEFAULT NULL,\n  `show_status` int(1) DEFAULT NULL,\n  `create_time` datetime DEFAULT NULL,\n  `read_count` int(1) DEFAULT NULL,\n  `content` text,\n  PRIMARY KEY (`id`)\n) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='帮助表';\n"
 	stmt, err := sqlparser.Parse(sql)
 	if err != nil {
 		// Do something with the err
+		fmt.Println(err)
 	}
 
 	// Otherwise do something with stmt
 	switch stmt := stmt.(type) {
-	case *sqlparser.Select:
-		_ = stmt
-	case *sqlparser.Insert:
 	case *sqlparser.DDL:
 		switch stmt.Action {
 		case "create":
-			fmt.Println("..")
+			var fields []string
+			for _, column := range stmt.TableSpec.Columns {
+				fields = append(fields, column.Name.String())
+			}
+
+			cocoStruct := CocoStruct{
+				name:   stmt.NewName.Name.String(),
+				fields: fields,
+			}
+
+			fmt.Println(cocoStruct)
 		}
-	default:
-		fmt.Println(stmt)
 	}
 }
