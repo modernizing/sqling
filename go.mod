5306f9b235375fa6abd4961856a32a779c9ba741
Phodal Huang <h@phodal.com>
2021-06-17 19:02:48 +0800

    refactor: try other parser

--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.16
 
 require (
 	github.com/iancoleman/strcase v0.1.3
-	github.com/pingcap/parser v0.0.0-20200623164729-3a18f1e5dceb // indirect
-	github.com/xwb1989/sqlparser v0.0.0-20180606152119-120387863bf2
+	github.com/pingcap/parser v0.0.0-20190613045206-37cc370a20a4
+	github.com/pingcap/tidb v0.0.0-20190625145607-60965b006877
 )
--- main.go
+++ main.go
@@ -2,55 +2,106 @@ package main
 
 import (
 	"fmt"
-	. "github.com/inherd/sqling/converter"
+	//. "github.com/inherd/sqling/converter"
 	. "github.com/inherd/sqling/model"
 	. "github.com/inherd/sqling/render"
-	"github.com/xwb1989/sqlparser"
+	"github.com/pingcap/parser"
+	"github.com/pingcap/parser/ast"
+	_ "github.com/pingcap/tidb/types/parser_driver"
 	"io/ioutil"
-	"os"
 )
 
 func main() {
-	args := os.Args
-	if len(args) < 2 {
-		return
-	}
-
-	dat, err := ioutil.ReadFile(args[1])
+	//args := os.Args
+	//if len(args) < 2 {
+	//	return
+	//}
+	//
+	//filename := args[1]
+	//
+	dat, err := ioutil.ReadFile("_fixtures/mall.sql")
 	Check(err)
 	sql := string(dat)
 
 	var structs []CocoStruct
-	structs = parseSql(sql, structs)
+	toStruct(sql, structs)
 
-	Write(structs)
+	// Write(structs)
 }
 
-func parseSql(sql string, structs []CocoStruct) []CocoStruct {
-	stmt, err := sqlparser.Parse(sql)
+type colX struct{
+	colNames []string
+}
+
+func (v *colX) Enter(in ast.Node) (ast.Node, bool) {
+	if name, ok := in.(*ast.ColumnName); ok {
+		fmt.Println(name)
+		v.colNames = append(v.colNames, name.Name.O)
+	}
+
+	fmt.Println(in.Text())
+	return in, false
+}
+
+func (v *colX) Leave(in ast.Node) (ast.Node, bool) {
+	return in, true
+}
+
+func extract(rootNode *ast.StmtNode) []string {
+	v := &colX{}
+	(*rootNode).Accept(v)
+	return v.colNames
+}
+
+
+func parse(sql string) (*ast.StmtNode, error) {
+	p := parser.New()
+
+	stmtNodes, _, err := p.Parse(sql, "", "")
 	if err != nil {
-		fmt.Println(err)
+		return nil, err
 	}
 
-	switch stmt := stmt.(type) {
-	case *sqlparser.DDL:
-		switch stmt.Action {
-		case "create":
-			var fields []CocoField
-			for _, column := range stmt.TableSpec.Columns {
-				fields = append(fields, CocoField{
-					Name:      column.Name.String(),
-					FieldType: FromMysqlType(column.Type.Type),
-				})
-			}
-
-			cocoStruct := CocoStruct{
-				Name:   stmt.NewName.Name.String(),
-				Fields: fields,
-			}
-
-			structs = append(structs, cocoStruct)
-		}
-	}
-	return structs
+	return &stmtNodes[0], nil
 }
+
+
+func toStruct(sql string, structs []CocoStruct) {
+	astNode, err := parse(sql)
+	if err != nil {
+		fmt.Printf("parse error: %v\n", err.Error())
+		return
+	}
+
+	extract(astNode)
+}
+
+//
+//func parseSql(sql string, structs []CocoStruct) []CocoStruct {
+//	stmt, err := sqlparser.Parse(sql)
+//	if err != nil {
+//		fmt.Println(err)
+//	}
+//
+//	switch stmt := stmt.(type) {
+//	case *sqlparser.DDL:
+//		switch stmt.Action {
+//		case "create":
+//			var fields []CocoField
+//			for _, column := range stmt.TableSpec.Columns {
+//				fields = append(fields, CocoField{
+//					Name:      column.Name.String(),
+//					FieldType: FromMysqlType(column.Type.Type),
+//				})
+//			}
+//
+//			cocoStruct := CocoStruct{
+//				Name:   stmt.NewName.Name.String(),
+//				Fields: fields,
+//			}
+//
+//			structs = append(structs, cocoStruct)
+//		}
+//	}
+//	return structs
+//}
