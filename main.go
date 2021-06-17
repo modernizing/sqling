e4133a6827660bf53812bcf04ced064b17d209c5
Phodal Huang <h@phodal.com>
2021-06-17 22:17:38 +0800

    refactor: fix comment issue

--- main.go
+++ main.go
@@ -5,17 +5,17 @@ import (
 	. "github.com/inherd/sqling/render"
 	_ "github.com/pingcap/tidb/types/parser_driver"
 	"io/ioutil"
+	"os"
 )
 
 func main() {
-	//args := os.Args
-	//if len(args) < 2 {
-	//	return
-	//}
-	//
-	//filename := args[1]
-	//
-	dat, err := ioutil.ReadFile("_fixtures/platform.sql")
+	args := os.Args
+	if len(args) < 2 {
+		return
+	}
+
+	filename := args[1]
+	dat, err := ioutil.ReadFile(filename)
 	Check(err)
 	sql := string(dat)
 
