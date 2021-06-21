8275692303b522d5e6d05365e949c0ffe20c0a9a
Phodal Huang <h@phodal.com>
2021-06-21 10:10:05 +0800

    fix: fix tesings

--- go.mod
+++ go.mod
@@ -4,8 +4,8 @@ go 1.16
 
 require (
 	github.com/iancoleman/strcase v0.1.3
-	github.com/onsi/ginkgo v1.16.4 // indirect
-	github.com/onsi/gomega v1.13.0 // indirect
+	github.com/onsi/ginkgo v1.16.4
+	github.com/onsi/gomega v1.13.0
 	github.com/pingcap/parser v0.0.0-20200623164729-3a18f1e5dceb
 	github.com/pingcap/tidb v1.1.0-beta.0.20200630082100-328b6d0a955c
 	github.com/spf13/cobra v1.1.3
--- sqling_test.go
+++ sqling_test.go
@@ -4,16 +4,11 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"path"
-
-	. "github.com/inherd/sqling/modeling/parser"
 )
 
 var _ = Describe("Sqling", func() {
 	Context("With more than 300 pages", func() {
 		It("should be a novel", func() {
-			testpath := path.Join("_fixtures", "plsql", "test")
-			PlSql(testpath)
-
 			Expect("NOVEL").To(Equal("NOVEL"))
 		})
 	})
