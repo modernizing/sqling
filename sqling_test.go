51d4801dcb124756e9933733073c70aa0205cbd0
Phodal Huang <h@phodal.com>
2021-06-23 09:33:36 +0800

    test: add first test for structs

--- sqling_test.go
+++ sqling_test.go
@@ -1,14 +1,35 @@
 package main_test
 
 import (
+	"github.com/inherd/sqling/modeling/parser"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Sqling", func() {
-	Context("With more than 300 pages", func() {
-		It("should be a novel", func() {
+	humanSql := `CREATE TABLE human(
+    id VARCHAR(12) PRIMARY KEY,
+    sname VARCHAR(12),
+    age INT,
+    sex CHAR(1)
+);`
+
+	Context("Structs generate", func() {
+		It("structs len & field len", func() {
 			Expect("NOVEL").To(Equal("NOVEL"))
+			structs, _ := parser.ParseSql(humanSql)
+			Expect(len(structs)).To(Equal(1))
+			fields := structs[0].Fields
+			Expect(len(fields)).To(Equal(4))
+		})
+
+		It("field content", func() {
+			Expect("NOVEL").To(Equal("NOVEL"))
+			structs, _ := parser.ParseSql(humanSql)
+			fields := structs[0].Fields
+
+			Expect(fields[0].Name).To(Equal("id"))
+			Expect(fields[0].FieldType).To(Equal("String"))
 		})
 	})
 })
