eba583e6e1c033a967efbd9a8bcfbb0d1e6b8dc1
Phodal Huang <h@phodal.com>
2021-06-23 09:38:18 +0800

    refactor: change test style close to golang

--- sqling_test.go
+++ sqling_test.go
@@ -2,34 +2,40 @@ package main_test
 
 import (
 	"github.com/inherd/sqling/modeling/parser"
-	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"testing"
 )
 
-var _ = Describe("Sqling", func() {
-	humanSql := `CREATE TABLE human(
+var humanSql = `CREATE TABLE human(
     id VARCHAR(12) PRIMARY KEY,
     sname VARCHAR(12),
     age INT,
     sex CHAR(1)
 );`
 
-	Context("Structs generate", func() {
-		It("structs len & field len", func() {
-			Expect("NOVEL").To(Equal("NOVEL"))
-			structs, _ := parser.ParseSql(humanSql)
-			Expect(len(structs)).To(Equal(1))
-			fields := structs[0].Fields
-			Expect(len(fields)).To(Equal(4))
-		})
+func Test_StructLen(t *testing.T) {
+	g := NewGomegaWithT(t)
+	structs, _ := parser.ParseSql(humanSql)
 
-		It("field content", func() {
-			Expect("NOVEL").To(Equal("NOVEL"))
-			structs, _ := parser.ParseSql(humanSql)
-			fields := structs[0].Fields
+	g.Expect(len(structs)).To(Equal(1))
+}
 
-			Expect(fields[0].Name).To(Equal("id"))
-			Expect(fields[0].FieldType).To(Equal("String"))
-		})
-	})
-})
+func Test_FieldsLen(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	structs, _ := parser.ParseSql(humanSql)
+
+	fields := structs[0].Fields
+	g.Expect(len(fields)).To(Equal(4))
+}
+
+func Test_StructField(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	structs, _ := parser.ParseSql(humanSql)
+	fields := structs[0].Fields
+
+	g.Expect(fields[0].Name).To(Equal("id"))
+	g.Expect(fields[0].FieldType).To(Equal("String"))
+}
+
