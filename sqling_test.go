ec2100efab31d9ef10e37f3f0ba43be64a3f15a8
Phodal Huang <h@phodal.com>
2021-06-25 10:13:47 +0800

    test: add refs testings

--- sqling_test.go
+++ sqling_test.go
@@ -13,6 +13,21 @@ var humanSql = `CREATE TABLE human(
     sex CHAR(1)
 );`
 
+var humanCarSql = `CREATE TABLE human(
+    id VARCHAR(12) PRIMARY KEY,
+    sname VARCHAR(12),
+    age INT,
+    sex CHAR(1)
+);
+
+CREATE TABLE car(
+    id VARCHAR(12) PRIMARY KEY,
+    mark VARCHAR(24),
+    price NUMERIC(6,2),
+    hid VARCHAR(12),
+    CONSTRAINT fk_human FOREIGN KEY(hid) REFERENCES human(id)
+);`
+
 func Test_StructLen(t *testing.T) {
 	g := NewGomegaWithT(t)
 	structs, _ := parser.ParseSql(humanSql)
@@ -37,5 +52,21 @@ func Test_StructField(t *testing.T) {
 
 	g.Expect(fields[0].Name).To(Equal("id"))
 	g.Expect(fields[0].FieldType).To(Equal("String"))
+}
+
+func Test_MultipleStructs(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	structs, _ := parser.ParseSql(humanCarSql)
+
+	g.Expect(len(structs)).To(Equal(2))
+}
+
+func Test_StructRefLen(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	_, refs := parser.ParseSql(humanCarSql)
+
+	g.Expect(len(refs)).To(Equal(1))
 }
 
